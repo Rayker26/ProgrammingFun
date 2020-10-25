0f86cf46b128e323adb3b81483fdc753f19384d0
Rayker26 <Rayker26@users.noreply.github.com>
2020-10-25 20:37:20 +0100

    test Two canvas

---
+++ sketchTree.js
@@ -0,0 +1,44 @@
+class branch{
+	constructor(x,y,length,dir){
+		this.length = length;
+		
+		line(x,y,x+Math.sign(dir)*(sqrt(Math.abs(dir))*length),y-((sqrt(1-Math.abs(dir)))*length))
+		if(length>=5){
+			new branch(x+Math.sign(dir)*(sqrt(Math.abs(dir))*length),y-((sqrt(1-Math.abs(dir))*length)),length*0.75,dir-angle);
+			new branch(x+Math.sign(dir)*(sqrt(Math.abs(dir))*length),y-((sqrt(1-Math.abs(dir))*length)),length*0.75,dir+angle);
+		}
+	}
+}
+angle = 0;
+timer = 0;
+fps = 0;
+let slider;
+function setup() 
+{
+	createCanvas(400, 400);
+	slider = createSlider(0,1,0,0.01);
+ 	slider.position(10, 10);
+  	slider.style('width', '80px');
+	
+	
+}
+
+function draw()
+{
+	angle = slider.value();
+	timer += deltaTime;
+	background(255);
+	if(timer>=1000){
+		fps = frameCount;
+		frameCount = 0;
+		timer = 0;
+		
+	}
+	
+	fill(200,0,0);
+	textSize(10);
+	text(fps,20,40);
+	fill(0);
+	new branch(200,400,75,0);
+	
+}
